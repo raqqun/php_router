e39bc173a54123a662eb4d2099b72f1439d950f7
Alexandros Nikiforidis <alexdeb@espiv.net>
2014-12-13 14:51:13 +0100

    commit base router class

---
+++ router.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+class Router {
+
+    protected $routes = array();
+
+
+    public function __construct() {
+
+    }
+
+    public function attach($method, $route, $controller) {
+
+    }
+
+
+    public function dispatch($route, $controller) {
+
+    }
+
+    public function default_404() {
+
+    }
+
+}
